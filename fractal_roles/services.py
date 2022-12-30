3d1defad62294957e60a77c0377d0ff3f3c6b976
Douwe van der Meij <douwe@karibu-online.nl>
2022-12-30 14:33:11 +0100

    split verify

--- fractal_roles/services.py
+++ fractal_roles/services.py
@@ -2,7 +2,7 @@ from abc import ABC
 from typing import List
 
 from fractal_roles.exceptions import NotAllowedException
-from fractal_roles.models import Role, TokenPayloadRolesMixin
+from fractal_roles.models import Methods, Role, TokenPayloadRolesMixin
 
 
 class Service(
@@ -23,10 +23,28 @@ class BaseRolesService(Service):
         self, payload: TokenPayloadRolesMixin, endpoint: str, method: str
     ) -> TokenPayloadRolesMixin:
         for role in self.roles:
-            if role.__class__.__name__.lower() in payload.roles:
-                if e := getattr(role, endpoint, None):
-                    if getattr(e, method, None):
-                        if m := getattr(e, method, None):
-                            payload.specification_func = m.specification_func
-                        return payload
+            if self._check_role(payload, endpoint, method, role):
+                return payload
         raise NotAllowedException("No permission!")
+
+    def _check_role(
+        self, payload: TokenPayloadRolesMixin, endpoint: str, method: str, role: Role
+    ):
+        if role.__class__.__name__.lower() in payload.roles:
+            if self._check_endpoint(payload, endpoint, method, role):
+                return payload
+
+    def _check_endpoint(
+        self, payload: TokenPayloadRolesMixin, endpoint: str, method: str, role: Role
+    ):
+        if methods := getattr(role, endpoint, None):
+            if self._check_method(payload, method, methods):
+                return payload
+
+    def _check_method(
+        self, payload: TokenPayloadRolesMixin, method: str, methods: Methods
+    ):
+        if getattr(methods, method, None):
+            if m := getattr(methods, method, None):
+                payload.specification_func = m.specification_func
+            return payload
